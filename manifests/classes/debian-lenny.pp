691d5188b82cb16aebf105589f28f99e29949eab
Marc Fournier <marc.fournier@camptocamp.com>
2011-07-15 11:38:27 +0200

    os::debian: manifest cleanup, removed a lot of duplicate/dead code. No feature change.

--- manifests/classes/debian-lenny.pp
+++ manifests/classes/debian-lenny.pp
@@ -1,37 +1,20 @@
-class os::debian-lenny inherits debian {
+class os::debian-lenny {
+
+  include os::debian
+
+  # Disable PC Speaker
+  line {"disable pc speaker":
+    line   => 'blacklist pcspkr',
+    file   => '/etc/modprobe.d/blacklist',
+    ensure => present,
+  }
+
   # general config for emacs (without temporary files ~ )
   file { "/etc/emacs/site-start.d/50c2c.el":
     ensure  => present,
     mode    => 644,
     source  => "puppet:///os/etc/emacs/site-start.d/50c2c.el",
     require => Package["emacs23-nox"]
-  }
-
-  # Umask, etc.
-  file { "/etc/profile":
-    ensure => present,
-    mode   => 644,
-    source => "puppet:///os/etc/profile-lenny",
-  }
-
-  # Timezone
-  file { "/etc/localtime":
-    ensure => present,
-    source => "file:///usr/share/zoneinfo/Europe/Zurich",
-  }
-
-  file { "/etc/timezone":
-    ensure  => present,
-    content => "Europe/Zurich",
-  }
-
-  # Kernel
-  file { "/etc/modules":
-    ensure => present,
-  }
-
-  package {["bash-completion", "iotop"]:
-    ensure => present,
   }
 
   apt::preferences { "c2c-mirror":
@@ -41,4 +24,65 @@ class os::debian-lenny inherits debian {
     priority => "1001",
   }
 
+  #
+  # Locales
+  #
+
+  package {"locales-all":
+    ensure => absent,
+  }
+
+  package {"locales":
+    ensure => present,
+    require => File["/etc/locale.gen"],
+    notify => Exec["locale-gen"],
+  }
+
+  file {"/etc/locale.gen":
+    ensure  => present,
+    source  => "puppet:///os/locale.gen",
+    notify => Exec["locale-gen"],
+  }
+
+  exec {"locale-gen":
+    refreshonly => true,
+    command => "locale-gen",
+    timeout => 30,
+    require => [File["/usr/share/locale/locale.alias"], Package["locales"], File["/etc/locale.gen"]],
+  }
+
+  # BUG: Smells hacky ?
+  file {"/usr/share/locale/locale.alias":
+    ensure => present,
+    source => "puppet:///os/locale.alias",
+  }
+
+  # SSL Configuration
+  package {
+    "ca-certificates": ensure => present;
+  }
+
+  # fix 7376
+  package { ["openssl", "openssh-server", "openssh-client", "openssh-blacklist", "ssl-cert" ]:
+    ensure => latest,
+    require => Exec["apt-get_update"]
+  }
+
+  exec {"sysctl-reload":
+    command     => "sysctl -p",
+    refreshonly => true,
+  }
+
+  # fix 14573
+  package {"debian-archive-keyring":
+    ensure => latest,
+  }
+
+  # fixes rt#14979
+  cron {"Keeps a fresh apt database":
+    command  => "/usr/bin/apt-get update -q=2",
+    ensure   => present,
+    hour     => 4,
+    minute   => ip_to_cron(1),
+  }
 }
--- manifests/classes/debian-squeeze.pp
+++ manifests/classes/debian-squeeze.pp
@@ -1,4 +1,7 @@
 class os::debian-squeeze {
+
+  include os::debian
+
   include locales::sources
   locales::locale {
     "de_DE.ISO-8859-1": charset => "ISO-8859-1";
@@ -20,93 +23,6 @@ class os::debian-squeeze {
     "en": locale => "en_US.ISO-8859-1";
     "fr": locale => "fr_FR.ISO-8859-1";
     "it": locale => "it_IT.ISO-8859-1";
-  }
-
-  # Umask, etc.
-  file { "/etc/profile":
-    ensure => present,
-    mode   => 644,
-    source => "puppet:///os/etc/profile-lenny",
-  }
-
-  file {"/etc/environment":
-    ensure => present,
-    content => '# File managed by puppet
-LANG="en_US.UTF-8"
-LC_NUMERIC="fr_CH.UTF-8"
-',
-    mode    => 0644,
-    owner   => root,
-    group   => root,
-  }
-
-  # Timezone
-  file { "/etc/localtime":
-    ensure => present,
-    source => "file:///usr/share/zoneinfo/Europe/Zurich",
-  }
-
-  file { "/etc/timezone":
-    ensure  => present,
-    content => "Europe/Zurich\n",
-  }
-
-  # Kernel
-  file { "/etc/modules":
-    ensure => present,
-  }
-
-  file {"/etc/adduser.conf":
-    ensure => present,
-    owner => root,
-    group => root,
-    mode => 644,
-    content => template("os/etc/adduser.conf.erb"),
-  }
-
-  package {
-    "bash-completion":  ensure => present;
-    "iotop":            ensure => present;
-    "lsof":             ensure => present;
-    "tiobench":         ensure => present; # Useful for doing IO benchmarks
-    "smartmontools":    ensure => present; # SMART monitoring
-    "at" :              ensure => present; # usefull for reboots...
-    "emacs23-nox" :     ensure => present; # for fredj (this comes from backports on Lenny)
-    "unzip":            ensure => present;
-    "zip":              ensure => present;
-    "strace":           ensure => present;
-    "iproute":          ensure => present;
-    "mtr-tiny":         ensure => present;
-    "tcpdump":          ensure => present;
-    "tshark":           ensure => present;
-    "curl":             ensure => present;
-    "screen":           ensure => present;
-    "vim":              ensure => present;
-    "lynx":             ensure => present;
-    "bzip2":            ensure => present;
-    "patch":            ensure => present;
-    "file":             ensure => present;
-    "less":             ensure => present;
-    "rsync":            ensure => present;
-    "rdiff-backup":     ensure => present;
-    "elinks":           ensure => present;
-    "psmisc":           ensure => present;
-    "nmap":             ensure => present;
-    "cadaver":          ensure => present;
-    "iptraf":           ensure => present;
-    "tofrodos":         ensure => present;
-    "bc":               ensure => present;
-    "whois":            ensure => present;
-    "ipcalc":           ensure => present;
-    "cvs":              ensure => present;
-    "gettext":          ensure => present;
-    "ngrep":            ensure => present;
-    "pwgen":            ensure => present;
-    "xfsprogs":         ensure => present;
-
-    "emacs21-common" :  ensure => absent;
-    "locate":           ensure => absent;
-    "cron-apt":         ensure => purged; # Keeps a fresh apt database
   }
 
 }
